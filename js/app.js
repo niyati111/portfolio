d86e66018da77e6aedb6da8923ed6c9b221c6a94
niyati850 <niyatitailor111@gmail.com>
2022-03-07 00:36:36 +0530

    code till about section

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
+
+filter_btns.forEach((btn) =>
+    btn.addEventListener("click", () => {
+        filter_btns.forEach((button) => button.classList.remove("active"));
+        btn.classList.add("active");
+
+        let filterValue = btn.dataset.filter;
+
+        $(".grid").isotope({ filter: filterValue});
+    })
+);
+
+$(".grid").isotope({
+    // options
+    itemSelector: ".grid-item",
+    layoutMode: "fitRows",
+    transitionDuration: "0.6s",
+});
+
+window.addEventListener("scroll", () => {
+    skillsEffect();
+    checkScroll(skills_wrap)
+})
+
+function skillsEffect() {
+    if (!checkScroll(skills_wrap)) return;
+    skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
+
+function checkScroll(el){
+    let rect = el.getBoundingClientRect();
+    if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+    return false;
+    console.log(rect.top);
+}
