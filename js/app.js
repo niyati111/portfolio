74d89b323e3ac79e981cb5f6f78ee434257bb5e8
niyati850 <niyatitailor111@gmail.com>
2022-03-08 23:53:17 +0530

    everything in the mobile part

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
+const records_wrap = document.querySelector(".records");
+const records_number = document.querySelectorAll(".number");
+const footer_input = document.querySelector(".footer-input");
+
+footer_input.addEventListener("focus", () => {
+    footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+    if(footer_input.value != "") return;
+    footer_input.classList.remove("focus");
+});
 
 filter_btns.forEach((btn) =>
     btn.addEventListener("click", () => {
@@ -21,6 +33,7 @@ $(".grid").isotope({
 });
 
 window.addEventListener("scroll", () => {
+    countup();
     skillsEffect();
     checkScroll(skills_wrap)
 })
@@ -36,3 +49,38 @@ function checkScroll(el){
     return false;
     console.log(rect.top);
 }
+
+function countup(){
+    if (!checkScroll(records_wrap)) return;
+    records_number.forEach(numb => {
+        const updateCount = () => {
+            let currentNum = +numb.innerText;
+            let maxNum = +numb.dataset.num;
+            let speed = 100;
+            const increment = Math.ceil(maxNum / speed);
+
+            if(currentNum < maxNum){
+                numb.innerText = currentNum + increment;
+                setTimeout(updateCount, 1);
+            }
+            else{
+                numb.innerText = maxNum;
+            }
+        };
+
+        setTimeout(updateCount, 400);
+    });
+}
+
+var mySwiper = new Swiper(".swiper-container", {
+    speed: 1100,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
